8ee929b2a4c5bd08e9f059524b7ea27b7535dbcf
dannyjknights <dannyjknights@gmail.com>
2023-10-09 16:35:31 +0100

    Tidy up

--- aws-hosts.tf
+++ aws-hosts.tf
@@ -2,9 +2,7 @@ resource "aws_instance" "boundary_public_target" {
   ami               = "ami-09ee0944866c73f62"
   instance_type     = "t2.micro"
   availability_zone = "eu-west-2b"
-  //user_data_base64  = data.cloudinit_config.ssh_trusted_ca.rendered
-  //key_name = aws_key_pair.ec2_key.key_name
-  key_name = "boundary"
+  key_name          = "boundary"
 
 
   network_interface {
@@ -24,21 +22,3 @@ resource "aws_network_interface" "boundary_public_target_ni" {
   private_ip_list_enabled = false
 
 }
-
-# data "cloudinit_config" "ssh_trusted_ca" {
-#   gzip          = false
-#   base64_encode = true
-
-#   part {
-#     content_type = "text/x-shellscript"
-#     content      = <<-EOF
-#     sudo curl -o /etc/ssh/trusted-user-ca-keys.pem \
-#     --header "X-Vault-Namespace: admin" \
-#     -X GET \
-#     ${var.vault_addr}/v1/ssh-client-signer/public_key
-#     sudo echo TrustedUserCAKeys /etc/ssh/trusted-user-ca-keys.pem >> /etc/ssh/sshd_config
-#     sudo systemctl restart sshd.service
-#     EOF
-
-#   }
-# }
