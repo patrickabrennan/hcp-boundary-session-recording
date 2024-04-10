74dc0e2e91a19b39580ffcf45dd703f5fb777072
Patrick Brennan <patrickabrennan@gmail.com>
2024-04-10 14:27:36 -0400

    Update credential-library.tf

--- credential-library.tf
+++ credential-library.tf
@@ -8,14 +8,15 @@ resource "vault_token" "boundary_vault_token" {
   period       = "24h"
 }
 
+#4-10-2024 #removed static key as using Vault
 //A native Boundary static credential store
-resource "boundary_credential_store_static" "static_cred_store" {
-  name        = "static_credential_store"
-  description = "Boundary Static Credential Store"
-  scope_id    = boundary_scope.project.id
-}
+#resource "boundary_credential_store_static" "static_cred_store" {
+#  name        = "static_credential_store"
+#  description = "Boundary Static Credential Store"
+#  scope_id    = boundary_scope.project.id
+#}
 
-4-10-2024 #removed static key as using Vault 
+#4-10-2024 #removed static key as using Vault 
 #resource "boundary_credential_ssh_private_key" "static_ssh_key" {
 #  credential_store_id = boundary_credential_store_static.static_cred_store.id
 #  private_key         = file("../boundary.pem")
